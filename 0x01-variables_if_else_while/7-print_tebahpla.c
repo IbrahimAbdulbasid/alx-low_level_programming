10b7c8d7a05d5a2c5358d2ad968fe6b95e23a2fd
IbrahimAbdulbasid <abdulbasidibrahim292@mail.com>
2022-10-17 22:09:34 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main -Entery point
+ *
+ * Return: Alway 0
+ *
+*/
+int main(void)
+{
+	char c;
+
+	c = 'z';
+	while
+		(c >= 'a') {
+			putchar(c)
+				c--;
+		}
+	putchar('\n');
+	return (0);
