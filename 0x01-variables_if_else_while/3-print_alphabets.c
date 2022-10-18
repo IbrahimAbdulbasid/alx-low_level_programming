9d84f7ea2554d989eac7e74309a8eabd9b935c0e
IbrahimAbdulbasid <abdulbasidibrahim292@mail.com>
2022-10-17 19:58:16 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ *main -Entery point
+ *
+ *Return: Always 0
+ *
+*/
+int main(void)
+{
+	char c;
+
+	char C;
+
+	c = 'a';
+
+	C = 'A';
+	while
+		(c <= 'z', C <= 'Z') {
+			putchar(c, C);
+			c++, C++;
+		}
+	putchar('\n');
+	return (0);
+}
