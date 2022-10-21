68ca3e5220ca4fb06ad48ebec6cacdcf3e892fca
IbrahimAbdulbasid <abdulbasidibrahim292@mail.com>
2022-10-20 19:31:40 -0700

    3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -11,9 +11,9 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
+	if ((year % 4 == 0) || (year % 400 == 0 && year % 100 == 0))
 	{
-		if (month >= 2 && day >= 60)
+		if (month >= 3 && day >= 60)
 		{
 			day++;
 		}
@@ -28,9 +28,8 @@ void print_remaining_days(int month, int day, int year)
 		}
 		else
 		{
-			printf("Days of the year: %d\n", day);
-			printf("Remaining days: %d\n",  365 - day);
+			printf("Day of the year: %d\n", day);
+			printf("Remaining days: %d\n", 365 - day);
 		}
 	}
-	return (day);
 }
