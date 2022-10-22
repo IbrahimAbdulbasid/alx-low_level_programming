3d084be526bacca3b0dd673e4780f008331b12e8
IbrahimAbdulbasid <abdulbasidibrahim292@mail.com>
2022-10-21 19:44:06 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - print numbers 10 times from 0-14
+*/
+void more_numbers(void)
+{
+	int n, count;
+
+	for (count = 0; count <= 9; count++)
+	{
+	for (n = 0; n <= 14; n++)
+	}
+	if (n > 9)
+	{
+	_putchar((n / 10) + '0');
+	}
+	{
+	_putchar((n % 10) + '0');
+	}
+	{
+	_putchar('\n');
+	}
+}
