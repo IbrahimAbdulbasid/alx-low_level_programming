45bb08744e4b508ab584dad11d81c322bc9faf86
IbrahimAbdulbasid <abdulbasidibrahim292@mail.com>
2022-11-01 15:36:13 -0700

    4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strpbrk - entery level
+ * @s: input
+ * @accept: input
+ * Return:Always 0
+*/
+char *_strpbrk(char *s, char *accept)
+{
+	int j;
+
+	while (*s)
+	{
+		for (k = 0; accept[j]; j++)
+		{
+			if (*s == accept[j])
+				return (s);
+		}
+		s++;
+	}
+	return ('\0');
+}
