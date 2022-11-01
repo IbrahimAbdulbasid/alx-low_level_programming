91fd909bad2a734918384169092dae22ae61e97e
IbrahimAbdulbasid <abdulbasidibrahim292@mail.com>
2022-10-31 19:32:57 -0700

    3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strspn - entery level
+ * @s: input
+ * @accept: input
+ * Return: always 0
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int b = 0;
+
+	int c;
+
+	while (*s)
+	{
+		for (c = 0; accept[c]; c++)
+		{
+			if (*s == accept[c])
+			{
+				b++;
+				break;
+			}
+			else if (accept[c + 1] == '\0')
+				return (n);
+		}
+		s++;
+	}
+	return (n);
+}
