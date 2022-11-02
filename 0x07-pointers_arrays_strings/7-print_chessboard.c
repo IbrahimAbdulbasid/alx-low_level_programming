585a1003101d78c8d846bf44a66a7e2e76e3daa9
IbrahimAbdulbasid <abdulbasidibrahim292@mail.com>
2022-11-01 17:03:13 -0700

    7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_chessboard - print the chessboard
+ * @a: the chessboard
+ * Return: void
+ */
+void print_chessboard(char (*a)[8]);
+{
+	int y;
+
+	int z;
+
+	for (y = 0; y < 8; y++)
+	{
+		for (z = 0; z < 8; z++)
+		{
+			_putchar(a[y][z]);
+		}
+		_putchar('\n');
+	}
+}
