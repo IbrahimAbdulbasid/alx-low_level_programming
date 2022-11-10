1fde9ce65c1d3676606f33fe71efb9ae18c913ac
IbrahimAbdulbasid <abdulbasidibrahim292@mail.com>
2022-11-09 17:28:57 -0800

    2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - print name of program
+ * @argc: count argument
+ * @argv: arguments
+ * Return: Always 0
+*/
+int main(int argc, char *argv[])
+{
+	int count = 0;
+
+	if (argc > 0)
+	{
+		while (count < argc)
+		{
+		printf("%s\n", argv[count]);
+		}
+	}
+}
