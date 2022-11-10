7c0258c96bfb7da16815094a3bab243276dc66ad
IbrahimAbdulbasid <abdulbasidibrahim292@mail.com>
2022-11-09 16:48:44 -0800

    main.h

---
+++ 0x0A-argc_argv/_putchar.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _putchar - write the chracter c to stdout
+ * @c: chracter to be printed
+ * Return: 1 on success
+*/
+int _putchar(char c)
+{
+	return (write(1, &c, 1);
+}
